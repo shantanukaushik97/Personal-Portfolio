ed14a4da22840985f69008592e2afbf9375d3980
SHANTANU KAUSHIK <84124506+shantanukaushik97@users.noreply.github.com>
2021-08-02 01:21:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web-Designer","Programmer", "Developer", "YouTuber", "Freelancer","AI & ML Engineer"],
+        strings: ["Web-Developer","Programmer", "AI Enthusiasts", "YouTuber", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web-Designer", "Programmer", "Developer", "YouTuber", "Freelancer","AI & ML Engineer"],
+        strings: ["Web-Developer", "Programmer", "AI Enthusiasts", "YouTuber", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
